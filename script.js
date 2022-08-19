638c0ca27052f18e9eccfb3b35dbfc282625ec03
Cris <cristinamihalciuc@yahoo.com>
2022-08-19 22:41:51 +0300

    Add render function to gameboard

--- script.js
+++ script.js
@@ -4,19 +4,29 @@ const gameboard = (() => {
     let x = 'x';
     let o = 'o';
     let e = 'e';
-    let gameBoard = [[x, o, o], [x, e, o], [o, x, e]];
+    let gameBoard = [[x, e, o], [x, e, o], [o, x, e]];
 
     const get = () => gameBoard;
     const play = (i, j, move) => gameBoard[i][j] = move;
     const reset = () => gameBoard = [[ , , ,], [ , , ,], [ , , ,]];
 
-    return{get, play, reset};
+    const render = () => {
+        let flatBoard = (gameboard.get()).flat();
+        for (let i = 0; i < flatBoard.length; i++) {
+            let cell = document.querySelector(`.gameboard :nth-child(${i+1})`);
+            let value = document.createTextNode(flatBoard[i]);
+            if (flatBoard[i] !== 'e') {
+                cell.appendChild(value);
+            }
+        }
+    }
+
+    return{get, play, reset, render};
 })();
 
+gameboard.render();
 
-let flatBoard = (gameboard.get()).flat();
-
-const cells = document.querySelectorAll(".cell");
+/* const cells = document.querySelectorAll(".cell");
 console.log(cells);
 let i = 0;
 cells.forEach((cell) => {
@@ -25,7 +35,7 @@ cells.forEach((cell) => {
     cell.appendChild(value);
     i++;
 });
-
+ */
 
 
 const Player = (name) => {
