c3acb62df0c3c8eb6c7f4a3061e49d4b61ba4afc
Cris <cristinamihalciuc@yahoo.com>
2022-08-20 15:11:15 +0300

    Add clearBoard method

--- script.js
+++ script.js
@@ -1,42 +1,46 @@
 
 
 const gameboard = (() => {
-    let x = 'x';
-    let o = 'o';
-    let e = 'e';
+    let x = 'X';
+    let o = 'O';
+    let e = ' ';
     let gameBoard = [[x, e, o], [x, e, o], [o, x, e]];
-
+    const flatBoard = gameBoard.flat();
+    
     const get = () => gameBoard;
     const play = (i, j, move) => gameBoard[i][j] = move;
-    const reset = () => gameBoard = [[ , , ,], [ , , ,], [ , , ,]];
+    const reset = () => {
+        gameBoard = [[e ,e ,e ,], [e ,e ,e ,], [e ,e ,e ,]];
+        
+    }
 
-    const render = () => {
-        let flatBoard = (gameboard.get()).flat();
+    const clearBoard = () => {
         for (let i = 0; i < flatBoard.length; i++) {
-            let cell = document.querySelector(`.gameboard :nth-child(${i+1})`);
-            let value = document.createTextNode(flatBoard[i]);
-            if (flatBoard[i] !== 'e') {
-                cell.appendChild(value);
-            }
+            let cell = document.querySelector(`.gameboard :nth-child(${i+1}) > p`);
+            cell.remove();
         }
     }
 
-    return{get, play, reset, render};
+    const render = () => {
+        for (let i = 0; i < flatBoard.length; i++) {
+            let cell = document.querySelector(`.gameboard :nth-child(${i+1})`);
+            let value = document.createElement('p');
+            value.textContent = flatBoard[i];
+            cell.appendChild(value);
+        }
+    }
+
+    /* const playerMove = () => {
+        for (let i = 0; i < flatBoard.length; i++) {
+            let cell = document.querySelector(`.gameboard :nth-child(${i+1})`);
+            cell.addEventListener('click', gameboard.play(1, 2, 'X'));
+        }
+    } */
+
+    return{get, play, reset, render, /* playerMove */ clearBoard};
 })();
 
 gameboard.render();
-
-/* const cells = document.querySelectorAll(".cell");
-console.log(cells);
-let i = 0;
-cells.forEach((cell) => {
-    
-    let value = document.createTextNode(`${flatBoard[i]}`);
-    cell.appendChild(value);
-    i++;
-});
- */
-
 
 const Player = (name) => {
     let points = 0;
@@ -50,3 +54,4 @@ const Player = (name) => {
 
     return {getName, getPoints, win}
 }
+
